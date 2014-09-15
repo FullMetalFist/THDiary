1f55af541a35b3484b175e6f7450309235d57a16
Michael Vilabrera <m.vilabrera@gmail.com>
2014-09-15 10:50:48 -0400

    Initial Commit

---
+++ THDiary/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  THDiary
+//
+//  Created by Michael Vilabrera on 9/15/14.
+//  Copyright (c) 2014 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ THDiaryTests/THDiaryTests.m
@@ -0,0 +1,34 @@
+//
+//  THDiaryTests.m
+//  THDiaryTests
+//
+//  Created by Michael Vilabrera on 9/15/14.
+//  Copyright (c) 2014 Giving Tree. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface THDiaryTests : XCTestCase
+
+@end
+
+@implementation THDiaryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
